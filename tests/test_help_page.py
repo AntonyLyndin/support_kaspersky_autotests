079d55d11f948776dfcdd288ee1fcf09c83c9601
anton_lyndin <lynant77@gmail.com>
2023-10-27 16:52:49 +0300

    Add test_check_sensor_ram_v30

--- tests/test_help_page.py
+++ tests/test_help_page.py
@@ -16,7 +16,17 @@ class TestHelpPage:
             'This text was not found'
 
     def test_check_sensor_ram_v30(self, driver):
-        pass
+        page = HelpPage(driver)
+        page.load_page()
+
+        page.open_industrial_cybersecurity_networks()
+        page.choose_3_0_version()
+        page.open_tab_hardware_software_requirements()
+
+        text = page.search_text()
+
+        assert text == 'RAM: 4 GB, and an additional 2 GB for each monitoring point on this computer.', \
+            'This text was not found'
 
     def test_check_sensor_ram_v40_rus(self, driver):
         pass
