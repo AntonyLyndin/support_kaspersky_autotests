7cd3583b39fbf4f142b9182e44ba8f9c5fafc549
anton_lyndin <lynant77@gmail.com>
2023-10-27 16:52:04 +0300

    Add test_check_sensor_ram_v40

--- tests/test_help_page.py
+++ tests/test_help_page.py
@@ -1,10 +1,19 @@
-from framework.pages.base_page import BasePage
+from framework.pages.help_page import HelpPage
 
 
 class TestHelpPage:
     def test_check_sensor_ram_v40(self, driver):
-        page = BasePage(driver)
+        page = HelpPage(driver)
         page.load_page()
+
+        page.open_industrial_cybersecurity_networks()
+        page.choose_4_0_version()
+        page.open_tab_hardware_software_requirements()
+
+        text = page.search_text()
+
+        assert text == 'RAM: 8 GB, and an additional 2 GB for each monitoring point on this computer', \
+            'This text was not found'
 
     def test_check_sensor_ram_v30(self, driver):
         pass
