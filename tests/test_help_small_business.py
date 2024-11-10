8bb07a867f3a9307e59d610ac62d8ee26aa9337c
lyndi <lyndin2001@gmail.com>
2024-11-10 11:01:52 +0300

    Aplay path file in master

---
+++ tests/test_help_small_business.py
@@ -0,0 +1,9 @@
+class TestHelpSmallBusinessPage:
+    def test_toolbar_main(self):
+        pass
+
+    def test_context_menu(self):
+        pass
+
+    def test_open_details_card(self):
+        pass
